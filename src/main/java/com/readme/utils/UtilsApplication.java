2d2db362345adf7daa35fa2e5286c608ee400b77
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-24 21:29:00 +0900

    [CREATE] BTS-142 utils 기본 설정
    - 개발 전 기본 설정

--- src/main/java/com/readme/utils/UtilsApplication.java
+++ src/main/java/com/readme/utils/UtilsApplication.java
@@ -3,9 +3,11 @@ package com.readme.utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableJpaAuditing
 public class UtilsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/readme/utils/messagequeue/KafkaConsumer.java
@@ -0,0 +1,33 @@
+package com.readme.utils.messagequeue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "test-topic")
+    public void addNovels(String kafkaMessage) {
+        log.info("kafka Message: -> " + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        log.info("map 데이터 : {}", map);
+        log.info("소설 id : {}", map.get("id"));
+
+
+    }
+}
