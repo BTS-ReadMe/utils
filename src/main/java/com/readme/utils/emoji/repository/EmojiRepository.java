2c90ba2da46dc97626e2746d0fca4796c5a77b60
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-29 20:36:45 +0900

    [CREATE] #BTS-149 이모지 조회 기능 개발
    - 이모지 조회 기능
    - 줄별로 이모지1, 이모지2, 이모지3 개수 출력

--- src/main/java/com/readme/utils/emoji/repository/EmojiRepository.java
+++ src/main/java/com/readme/utils/emoji/repository/EmojiRepository.java
@@ -1,11 +1,26 @@
 package com.readme.utils.emoji.repository;
 
+import com.readme.utils.emoji.dto.EmojiGroupDto;
 import com.readme.utils.emoji.model.Emoji;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmojiRepository extends JpaRepository<Emoji, Long> {
 
     boolean existsByUuidAndEpisodeIdAndEpisodeRow(String uuid, long episodeId, int episodeRow);
 
     Emoji findByUuidAndEpisodeIdAndEpisodeRow(String uuid, long episodeId, int episodeRow);
+
+//    @Query(value = "SELECT episode_id, episode_row, emoji, COUNT(emoji) FROM emoji "
+//        + "WHERE episode_id = ? GROUP BY emoji, episode_row ORDER BY episode_row",
+//        nativeQuery = true)
+    @Query("SELECT new com.readme.utils.emoji.dto.EmojiGroupDto(e.episodeId, e.episodeRow, e.emoji, COUNT(e.emoji)) "
+        + "FROM Emoji e "
+        + "WHERE e.episodeId = :episodeId "
+        + "GROUP BY e.emoji, e.episodeRow "
+        + "ORDER BY e.episodeRow")
+    List<EmojiGroupDto> getEmojiGroups(@Param("episodeId") Long episodeId);
+
 }
--- src/main/java/com/readme/utils/emoji/service/EmojiServiceImpl.java
+++ src/main/java/com/readme/utils/emoji/service/EmojiServiceImpl.java
@@ -1,14 +1,21 @@
 package com.readme.utils.emoji.service;
 
 import com.readme.utils.emoji.dto.EmojiDto;
+import com.readme.utils.emoji.dto.EmojiGroupDto;
 import com.readme.utils.emoji.model.Emoji;
 import com.readme.utils.emoji.repository.EmojiRepository;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmojiServiceImpl implements EmojiService {
 
     private final EmojiRepository emojiRepository;
@@ -28,5 +35,31 @@ public class EmojiServiceImpl implements EmojiService {
             Emoji emoji = new Emoji(emojiDto);
             emojiRepository.save(emoji);
         }
+    }
+
+    @Override
+    public Map<Long, Map<String, Long>> getEmojiByEpisodeId(String uuid, Long episodeId) {
+        List<EmojiGroupDto> list = emojiRepository.getEmojiGroups(episodeId);
+
+        Map<Long, Map<String, Long>> resultMap = new HashMap<>();
+
+        list.forEach(emojiGroupDto -> {
+            Map<String, Long> map = new HashMap<>();
+            map.put("emoji1", 0L);
+            map.put("emoji2", 0L);
+            map.put("emoji3", 0L);
+
+            resultMap.put((long)emojiGroupDto.getEpisodeRow(), map);
+        });
+
+        list.forEach(emojiGroupDto -> {
+            String key = "emoji"+emojiGroupDto.getEmoji();
+            Long value = emojiGroupDto.getCount();
+            resultMap.get((long)emojiGroupDto.getEpisodeRow()).merge(key, value, Long::sum);
+        });
+
+        log.info("결과!!! : " + resultMap);
+
+        return resultMap;
     }
 }
