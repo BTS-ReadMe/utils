e4e2b2f3a53d243ad074f96de773f95dabe8114d
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-31 22:02:33 +0900

    [UPDATE] #BTS-149 emoji 조회 수정
    - 이모지 조회시 uuid 필수값 아니게 수정
    (로그인 아니어도 조회 가능)

--- src/main/java/com/readme/utils/emoji/controller/EmojiController.java
+++ src/main/java/com/readme/utils/emoji/controller/EmojiController.java
@@ -5,6 +5,9 @@ import com.readme.utils.emoji.dto.EmojiDto;
 import com.readme.utils.emoji.requestObject.RequestAddEmoji;
 import com.readme.utils.emoji.responseObject.ResponseEmoji;
 import com.readme.utils.emoji.service.EmojiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -26,15 +29,29 @@ public class EmojiController {
 
     private final EmojiService emojiService;
 
+    @Operation(summary = "이모지 등록/수정", description = "이모지 등록/수정 (로그인 필요)", tags = {"이모지"})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @PostMapping
     public void addEmoji(@RequestHeader("uuid") String uuid,
         @RequestBody RequestAddEmoji requestAddEmoji) {
         emojiService.addEmoji(new EmojiDto(uuid, requestAddEmoji));
     }
 
+    @Operation(summary = "이모지 조회", description = "이모지 조회 (로그인 필수x, 로그인시 본인 체크한 거 표시)", tags = {"이모지"})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/{episodeId}")
     public ResponseEntity<CommonResponseData<List<ResponseEmoji>>> getEmojiByEpisodeId(
-        @RequestHeader("uuid") String uuid, @PathVariable Long episodeId) {
+        @RequestHeader(value = "uuid",required = false, defaultValue = "") String uuid, @PathVariable Long episodeId) {
 
         List<ResponseEmoji> emojiList = emojiService.getEmojiByEpisodeId(uuid, episodeId);
 
